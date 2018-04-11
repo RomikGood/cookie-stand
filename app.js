61431123bd653e3f82e70a01a1eda306e64921b2
Roman Kireev <kireev@gmail.com>
2018-04-11 07:37:01 -0700

    final version

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ function CookieStore (storeName, minCust, maxCust, aveSale) {
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.aveSale = aveSale;
-  this.cookieDay = 0; // total amount of cookies sold per day
-  this.numCookieSold = []; // amount of cookies sold per hour
-  CookieStore.allStores.push(this);
+  this.cookieDay = 0;                  // total amount of cookies sold per day
+  this.numCookieSold = [];             // array of amount of cookies sold per hour
+  CookieStore.allStores.push(this);    //array of all cookie stores
 }
 CookieStore.prototype.rendomNumCust = function(){
   return Math.random() * (this.maxCust - this.minCust) + this.minCust; //Generates random number of customers in given range
@@ -18,83 +18,63 @@ CookieStore.prototype.cookieHour = function(){                                  
     this.numCookieSold.push(Math.round(this.rendomNumCust() * this.aveSale));   //rounds up the value and pushes it into array
   }
 };
-CookieStore.prototype.totalCookieSold = function () {                                       // total cookies sold per day. adds each array value
+CookieStore.prototype.totalCookieSold = function () {       // total cookies sold per day. adds each array value
   for (var i= 0; i < this.numCookieSold.length; i++) {
     this.cookieDay += this.numCookieSold[i];
   }
 };
-CookieStore.prototype.renderStore = function() {                                        //creates an anchor firstPike for html page
-  var ulElem = document.getElementById(this.storeName);                  //creates a combined list from two arrays
-  for (var i = 0; i < this.numCookieSold.length; i++) {               //open hours and cookies sold in this hour.
-    var liElem = document.createElement('li');
-    liElem.textContent = CookieStore.openHours[i] + ' : ' + this.numCookieSold[i]; //parentElement.appendChild(childElement);
-    ulElem.appendChild(liElem);
-  }
 
-  liElem.textContent = ' Total : ' + this.cookieDay;                 // takes total amount cookieDay and put it at the end of the list
-  ulElem.appendChild(liElem);                                        // parentElement.appendChild(childElement);
-};
-
-var CookieStoreTable = document.getElementById('cookieStore');
+var CookieStoreTable = document.getElementById('cookieStore');  // we access table in the DOM (html)
 
 CookieStore.prototype.renderStore = function() {
+
   var trElement = document.createElement('tr'); // create tr
-  var tdElement = document.createElement('td'); // create td
-  tdElement.textContent = this.storeName; //content
-  trElement.appendChild(tdElement); //append to tr
+  var tdElement = document.createElement('th'); // create td / th
+  
+  tdElement.textContent = this.storeName; //give td content (name of each store)
+  trElement.appendChild(tdElement); //append td to tr
 
   for (var i = 0; i < this.numCookieSold.length; i++) {   //
-    var cookieSoldData = document.createElement('td');    //
-    cookieSoldData.textContent = this.numCookieSold[i];   //
-    trElement.appendChild(cookieSoldData);               //
+    var cookieSoldData = document.createElement('td');    // create td
+    cookieSoldData.textContent = this.numCookieSold[i];   // takes data from i element of arrany # cookies sold per hour
+    trElement.appendChild(cookieSoldData);               // append this data to tr
   }
 
-  var totalElement = document.createElement('td');         //
-  totalElement.textContent = this.cookieDay;               //
-  trElement.appendChild(totalElement);                     //
+  var totalElement = document.createElement('th');         //create td (or th) for total
+  totalElement.textContent = this.cookieDay;               //give td content of total sold per day
+  trElement.appendChild(totalElement);                     //append this total td to tr
 
 
-  CookieStoreTable.appendChild(trElement);
+  CookieStoreTable.appendChild(trElement);          //append tr to table 
 };
 
+function renderHeader() {   // create header row function
 
-function renderHeader() {
-  // create a row
-  var headerRow = document.createElement('tr');
+  var headerRow = document.createElement('tr'); // create tr
 
   var headings = ['','6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','Total'];
   for(var i of headings) {
-    var thElement = document.createElement('th');
-
-    // fill in their content
-    // Note, if using "for(var i in headings)" then the following line would be written as:
-    //    "thElement.textContent = headings[i]"
-    thElement.textContent = [i];
-
-    //append th to headerRow
-    headerRow.appendChild(thElement);
+    var thElement = document.createElement('th');  // creat th (or td) for each hour
+    thElement.textContent = [i];  // fill in their content
+    headerRow.appendChild(thElement);  //append th to headerRow
   }
 
-  // append to the table
-  CookieStoreTable.appendChild(headerRow);
+  CookieStoreTable.appendChild(headerRow); // append hours row to the table
 }
 
+renderHeader(); // call function to create header row hours 
 
-
-new CookieStore ('firstPike', 23, 65, 6.3);
+new CookieStore ('firstPike', 23, 65, 6.3); // create new object
 new CookieStore ('seaTac', 3, 24, 1.2);
 new CookieStore ('capHill', 23, 65, 6.3);
 new CookieStore ('seaCenter', 11, 38, 3.7);
 new CookieStore ('alkiStore', 2, 16, 4.6);
-//console.log(CookieStore.allStores[0]);
 
-renderHeader();
-
-for(var i = 0; i < CookieStore.allStores.length; i++){
-  console.log(CookieStore.allStores[i]);
+for(var i = 0; i < CookieStore.allStores.length; i++){   // calclates all functions for each store
+  CookieStore.allStores[i].rendomNumCust();
   CookieStore.allStores[i].cookieHour();
   CookieStore.allStores[i].totalCookieSold();
-  CookieStore.allStores[i].renderStore();
+  CookieStore.allStores[i].renderStore(); // call function to append data to the table
 }
 
 
